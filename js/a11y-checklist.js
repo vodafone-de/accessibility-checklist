e717e8565cda9dcd188cc395b04b96d5e8c62fac
vodafone-de <37216569+vodafone-de@users.noreply.github.com>
2022-10-04 22:42:23 +0200

    update working result

--- js/a11y-checklist.js
+++ js/a11y-checklist.js
@@ -17,11 +17,13 @@ if ($('body').hasClass('dark')) {
 }); 
 
 
+
 // Big thanks to Bastian Pertz for help with the following function
 
 var numberOfRelevantCheckboxes = 0;
 var numberOfUnrelevantCheckboxes = 0;
 var numberOfCheckedRelevantCheckboxes = 0;
+
 
 var updateStatus = function(){
     $('#count-checked-checkboxes').text(numberOfCheckedRelevantCheckboxes);
@@ -58,8 +60,6 @@ var updateStatus = function(){
     else {
       
     }
-
-
     
 };
 
@@ -69,8 +69,7 @@ var $checkboxes = $('input:checkbox');
     $checkboxes.each(function(){
 
       if('relevant' !== $(this).attr('data-dis')){
-        numberOfRelevantCheckboxes += 1;
-        
+        numberOfRelevantCheckboxes += 1;       
       }
     });
 
@@ -84,6 +83,7 @@ var $checkboxes = $('input:checkbox');
         
         if('relevant' === $checkbox.attr('data-dis')){
           isRelevant = false;
+          
         }
 
         var isChecked = $checkbox.is(':checked');
@@ -137,6 +137,7 @@ var $checkboxes = $('input:checkbox');
 
           var ruletog = $(this).data('ruletog');
           $("[data-ruledis='" + ruletog + "']").attr("disabled", null);
+          
 
         }
 
@@ -147,6 +148,51 @@ var $checkboxes = $('input:checkbox');
     });
 
 
+
+    $(".result").click(function() {
+
+      var rule = $('input[name="a11y-rule"]');
+      var chtml = "<h4>Considered</h4><ul class='checkmark'>";
+      var uchtml = "<h4>Needs to be improved</h4><ul class='fail'>";
+      var dischtml = "<h4>Not relevant</h4><ul class='fail'>";
+    
+      $.each(rule, function() {
+        var $this = $(this);
+    
+        if ($this.is(":checked") && !$($this).prop('disabled')) {
+          
+          chtml += "<li>"+$this.val()+" </li>";
+         
+        }
+
+        
+        if (!$this.is(":checked") && !$($this).prop('disabled')) {
+         
+          uchtml += "<li class='error-circle'><span class='ect'>" + $this.val() + " </span></li>";
+          
+          
+        }
+    
+        if (!$this.is(":checked") && $($this).prop('disabled')) {
+        dischtml += "<li class='disable-circle'><span class='ect'>" + $this.val() + " </span></li>";
+         
+        }
+    
+    });
+    
+    
+    
+    chtml += "</ul>";
+    
+    
+     $("#resultchecked").html(chtml);
+     $("#resultunchecked").html(uchtml);
+     $("#rulesdisabled").html(dischtml);
+    
+    
+    });
+    
+    
 
  
 
@@ -191,172 +237,6 @@ $('.reset-form').click(function(event) {
 
   });
  */
-
-
-
-/**
- * Checks that an element has a non-empty `name` and `value` property.
- * @param  {Element} element  the element to check
- * @return {Bool}             true if the element is an input, false if not
- */
- var isValidElement = function isValidElement(element) {
-  return element.name && element.value;
-};
-
-/**
- * Checks if an element’s value can be saved (e.g. not an unselected checkbox).
- * @param  {Element} element  the element to check
- * @return {Boolean}          true if the value should be added, false if not
- */
-var isValidValue = function isValidValue(element) {
-  return !['checkbox', 'radio'].includes(element.type) || element.checked;
-};
-
-/**
- * Checks if an input is a checkbox, because checkboxes allow multiple values.
- * @param  {Element} element  the element to check
- * @return {Boolean}          true if the element is a checkbox, false if not
- */
-var isCheckbox = function isCheckbox(element) {
-  return element.type === 'checkbox';
-};
-
-/**
- * Checks if an input is a `select` with the `multiple` attribute.
- * @param  {Element} element  the element to check
- * @return {Boolean}          true if the element is a multiselect, false if not
- */
-var isMultiSelect = function isMultiSelect(element) {
-  return element.options && element.multiple;
-};
-
-/**
- * Retrieves the selected options from a multi-select as an array.
- * @param  {HTMLOptionsCollection} options  the options for the select
- * @return {Array}                          an array of selected option values
- */
-var getSelectValues = function getSelectValues(options) {
-  return [].reduce.call(options, function (values, option) {
-    return option.selected ? values.concat(option.value) : values;
-  }, []);
-};
-
-/**
- * A more verbose implementation of `formToJSON()` to explain how it works.
- *
- * NOTE: This function is unused, and is only here for the purpose of explaining how
- * reducing form elements works.
- *
- * @param  {HTMLFormControlsCollection} elements  the form elements
- * @return {Object}                               form data as an object literal
- */
-var formToJSON_deconstructed = function formToJSON_deconstructed(elements) {
-
-  // This is the function that is called on each element of the array.
-  var reducerFunction = function reducerFunction(data, element) {
-
-    // Add the current field to the object.
-    data[element.name] = element.value;
-
-    // For the demo only: show each step in the reducer’s progress.
-    console.log(JSON.stringify(data));
-
-    return data;
-  };
-
-  // This is used as the initial value of `data` in `reducerFunction()`.
-  var reducerInitialValue = {};
-
-  // To help visualize what happens, log the inital value, which we know is `{}`.
-  console.log('Initial `data` value:', JSON.stringify(reducerInitialValue));
-
-  // Now we reduce by `call`-ing `Array.prototype.reduce()` on `elements`.
-  var formData = [].reduce.call(elements, reducerFunction, reducerInitialValue);
-
-  // The result is then returned for use elsewhere.
-  return formData;
-};
-
-/**
- * Retrieves input data from a form and returns it as a JSON object.
- * @param  {HTMLFormControlsCollection} elements  the form elements
- * @return {Object}                               form data as an object literal
- */
-var formToJSON = function formToJSON(elements) {
-  return [].reduce.call(elements, function (data, element) {
-
-    // Make sure the element has the required properties and should be added.
-    if (isValidElement(element) && isValidValue(element)) {
-
-      /*
-       * Some fields allow for more than one value, so we need to check if this
-       * is one of those fields and, if so, store the values as an array.
-       */
-      if (isCheckbox(element)) {
-        data[element.name] = (data[element.name] || []).concat(element.value);
-      } else if (isMultiSelect(element)) {
-        data[element.name] = getSelectValues(element);
-      } else {
-        data[element.name] = element.value;
-      }
-    }
-
-    return data;
-  }, {});
-};
-
-/**
- * A handler function to prevent default submission and run our custom script.
- * @param  {Event} event  the submit event triggered by the user
- * @return {void}
- */
-var handleFormSubmit = function handleFormSubmit(event) {
-
-  // Stop the form from submitting since we’re handling that with AJAX.
-  event.preventDefault();
-
-  // Call our function to get the form data.
-  var data = formToJSON(form.elements);
-
-  // Demo only: print the form data onscreen as a formatted JSON object.
-  var dataContainer = document.getElementsByClassName('results__display')[0];
-
-  // Use `JSON.stringify()` to make the output valid, human-readable JSON.
-  dataContainer.textContent = JSON.stringify(data, null, "  ");
-
-
-
-  // ...this is where we’d actually do something with the form data...
-  downloadInnerHtml(fileName, 'JsonCompiled','text/html');
-
-
-
-};
-
-/*
- * This is where things actually get started. We find the form element using
- * its class name, then attach the `handleFormSubmit()` function to the 
- * `submit` event.
- */
-var form = document.getElementsByClassName('contact-form')[0];
-form.addEventListener('submit', handleFormSubmit);
-
-function downloadInnerHtml(filename, elId, mimeType) {
-    var elHtml = document.getElementById(elId).innerHTML;
-    var link = document.createElement('a');
-    mimeType = mimeType || 'text/plain';
-
-    link.setAttribute('download', filename);
-    link.setAttribute('href', 'data:' + mimeType + ';charset=utf-8,' + encodeURIComponent(elHtml));
-    link.click(); 
-}
-
-var fileName =  'WebValues.json'; // You can use the .txt extension if you want
-
-$('#downloadLink').click(function(){
-    downloadInnerHtml(fileName, 'JsonCompiled','text/html');
-});
-
 
 
 
@@ -426,43 +306,6 @@ $(document).keyup(function(e) {
 
 
 
-    $(".result").click(function() {
-
-      var rule = $('input[name="a11y-rule"]');
-      var chtml = "<h4>Passed</h4><ul class='checkmark'>";
-      var uchtml = "<h4>Failed</h4><ul class='fail'>";
-      var dischtml = "<h4>Not relevant</h4><ul class='fail'>";
-
-      $.each(rule, function() {
-        var $this = $(this);
-
-        if ($this.is(":checked") && !$($this).prop('disabled')) {
-          
-          chtml += "<li>"+$this.val()+" </li>";
-        $("#resultchecked").html(chtml);
-        
-        }
-        
-        if (!$this.is(":checked") && !$($this).prop('disabled')) {
-         
-          uchtml += "<li class='error-circle'><span class='ect'>" + $this.val() + " </span></li>";
-          $("#resultunchecked").html(uchtml);
-          
-        }
-
-        if (!$this.is(":checked") && $($this).prop('disabled')) {
-        dischtml += "<li class='disable-circle'><span class='ect'>" + $this.val() + " </span></li>";
-          $("#rulesdisabled").html(dischtml);
-        }
-  
-});
-
-
-
-    chtml += "</ul>";
-
-
-    });
 
 
 
