aadc0fdae30fdba48c03dc5d27052619a1a621ab
vodafone-de <jonas.fuchs@vodafone.com>
2024-06-11 18:01:04 +0200

    changelog: code refactoring (still has issues in sorting)

--- js/changelog.js
+++ js/changelog.js
@@ -4,14 +4,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const changelogElement = document.getElementById('changelog');
     const orderSelect = document.getElementById('order');
 
-    let version = { major: 3, minor: 0, fix: 4 };
+    let version = { major: 0, minor: 0, fix: 0 };
     let commits = [];
     let versions = [];
 
+    const sortStrategies = {
+        oldest: (commits) => [...commits].reverse(),
+        messageLength: (commits) => [...commits].sort((a, b) => a.commit.message.length - b.commit.message.length),
+        customTag: (commits) => [...commits].sort((a, b) => {
+            const aTag = a.commit.message.match(/#(\w+)/);
+            const bTag = b.commit.message.match(/#(\w+)/);
+            return (aTag ? aTag[1] : '').localeCompare(bTag ? bTag[1] : '');
+        })
+    };
+
     async function fetchCommits() {
         const response = await fetch(repoUrl);
-        const commits = await response.json();
-        return commits;
+        return await response.json();
     }
 
     function updateVersion(commitMessage) {
@@ -33,30 +42,57 @@ document.addEventListener('DOMContentLoaded', () => {
         versionElement.textContent = `Version: ${version.major}.${version.minor}.${version.fix}`;
     }
 
+    function sortCommits(commits, order) {
+        if (sortStrategies[order]) {
+            return sortStrategies[order](commits);
+        }
+        return commits;
+    }
+
+    function createVersionHeader(version) {
+        const versionHeader = document.createElement('h5');
+        versionHeader.textContent = `Version ${version.major}.${version.minor}`;
+        versionHeader.classList.add('white-text');
+        return versionHeader;
+    }
+
     function displayChangelog(commits, order = 'oldest') {
         changelogElement.innerHTML = '';
-        const sortedCommits = [...commits];
-        if (order === 'oldest') {
-            sortedCommits.reverse();
-        }
+        const sortedCommits = sortCommits(commits, order);
 
         let currentVersionIndex = -1;
-        sortedCommits.forEach(commit => {
+        let previousChanges = [];
+        let isPreviousChangesSectionAdded = false;
+
+        sortedCommits.forEach((commit, index) => {
             const commitMessage = commit.commit.message;
 
             if (commitMessage.startsWith('Major') || commitMessage.startsWith('Minor')) {
                 currentVersionIndex++;
-                const versionHeader = document.createElement('h5');
-                 versionHeader.classList.add('white-text');
-                const version = versions[currentVersionIndex];
-                versionHeader.textContent = `Version ${version.major}.${version.minor}.${version.fix}`;
+                const versionHeader = createVersionHeader(versions[currentVersionIndex]);
                 changelogElement.appendChild(versionHeader);
             }
 
             const li = document.createElement('li');
             li.textContent = commitMessage;
-            changelogElement.appendChild(li);
+
+            if (commitMessage.startsWith('Major') || commitMessage.startsWith('Minor')) {
+                changelogElement.appendChild(li);
+            } else {
+                previousChanges.push(li);
+            }
         });
+
+        if (previousChanges.length > 0) {
+            const previousHeader = document.createElement('h5');
+            previousHeader.textContent = 'Previous changes';
+            previousHeader.classList.add('white-text');
+            changelogElement.appendChild(previousHeader);
+
+            previousChanges.forEach(li => {
+                changelogElement.appendChild(li);
+            });
+        }
     }
 
     async function generateChangelog() {
